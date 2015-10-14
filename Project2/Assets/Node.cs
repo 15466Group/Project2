2e6d08cdee563f05ed64c83a8dd1fb4d18fddc05
dominicxcliu <dominicxcliu@gmail.com>
2015-10-14 19:32:17 -0400

    basic grid

---
+++ Project2/Assets/Node.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : Object {
+
+	public bool open;
+	public Vector3 loc;
+
+	public Node (bool op, Vector3 pos) {
+		open = op;
+		loc = pos;
+	}
+
+}
