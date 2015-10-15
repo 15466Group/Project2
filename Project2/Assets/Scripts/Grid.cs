f4d8ee4e1b86ccbafe36e9187e86c7096d19b670
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2015-10-15 18:16:45 -0400

    some edits for the scenes and stuff

---
+++ Project2/Assets/Scripts/Grid.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public float nodeSize;
+
+	private Node[,] grid;
+	private int gridWidth;
+	private int gridHeight;
+	private float worldWidth;
+	private float worldHeight;
+	private Vector3 worldNW; //world north west, top left corner of map/plane
+
+
+	// Use this for initialization
+	void Start () {
+		worldWidth = transform.lossyScale.x * 10.0f; //plane
+		worldHeight = transform.lossyScale.z * 10.0f; //plane
+		Debug.Log (worldHeight + " + " + worldWidth);
+
+		gridWidth = Mathf.RoundToInt(worldWidth / nodeSize);
+		gridHeight = Mathf.RoundToInt(worldHeight / nodeSize);
+
+		grid = new Node[gridWidth, gridHeight];
+
+		worldNW = transform.position - (transform.right * worldWidth / 2.0f) + (transform.forward * worldHeight / 2.0f);
+
+		updateGrid ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateGrid ();
+	}
+
+	void updateGrid(){
+		for (int i = 0; i < gridWidth; i++) {
+			for (int j = 0; j < gridHeight; j ++) {
+				float xp = i * nodeSize + (nodeSize/2.0f) + worldNW.x;
+				float zp = -(j * nodeSize + (nodeSize/2.0f)) + worldNW.z;
+				Vector3 nodeCenter = new Vector3(xp, 0.0f, zp);
+				Collider[] hits = Physics.OverlapSphere(nodeCenter, nodeSize/2.0f);
+				if(hits.Length == 0) {
+					grid[i,j] = new Node(true, nodeCenter);
+				}
+				else {
+					grid[i,j] = new Node(false, nodeCenter);
+				}
+			}
+		}
+	}
+
+//	void OnDrawGizmos() {
+//		for (int i = 0; i < gridWidth; i++) {
+//			for (int j = 0; j < gridHeight; j ++) {
+//				if (!grid [i, j].open) {
+//					Gizmos.color = Color.red;
+//					Gizmos.DrawCube (grid [i, j].loc, new Vector3 (nodeSize, 1.0f, nodeSize));
+//				}
+//			}
+//		}
+//	}
+
+
+}
---
+++ Project2/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMovement : MonoBehaviour {
+
+	public string direction;
+	private float movementTime;
+	private float howLong;
+
+	// Use this for initialization
+	void Start () {
+		movementTime = 0.0f;
+		howLong = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movementTime += Time.deltaTime;
+		if (movementTime <= howLong) {
+			if (string.Compare (direction, "X") == 0) {
+				transform.position += transform.right;
+			}
+			else{
+				transform.position += transform.forward;
+			}
+		} else {
+			if (movementTime <= 2*howLong){
+				if (string.Compare (direction, "X") == 0) {
+					transform.position -= transform.right;
+				}
+				else{
+					transform.position -= transform.forward;
+				}
+			}
+			else {
+				movementTime = 0.0f;
+			}
+		}
+	}
+}
